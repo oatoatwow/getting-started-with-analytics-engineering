0454c1e2626daffe0bbd4924f319aed7f92a69dd
oatoatwow <123962118+oatoatwow@users.noreply.github.com>
2023-01-31 05:03:17 +0000

    Project version 1

---
+++ greenery/models/user_with_purchase.sql
@@ -0,0 +1,16 @@
+with user_with_purchase as (
+select 
+  user_id
+  ,case
+  when count(order_id) = 1 then 'one_purchase'
+  when count(order_id) = 2 then 'tow_purchase'
+  when count(order_id) >= 3 then 'three_plus_purchase'
+  end as puchase
+from public.orders
+group by 1
+)
+
+select puchase
+,count(user_id) as user_count
+from user_with_purchase
+group by 1
